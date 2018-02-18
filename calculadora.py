09ed84ac9aa24047972a5e049fb3ea1987a8212e
LydiaGarrido <l.garridomu@alumnos.urjc.es>
2018-02-18 21:11:46 +0100

    Calculadora mejorada

---
+++ calculadora.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def suma(operando1, operando2):
+    return operando1 + operando2
+
+
+def resta(operando1, operando2):
+    return operando1 - operando2
+
+
+def multiplicacion(operando1, operando2):
+    return operando1 * operando2
+
+
+def division(operando1, operando2):
+    return operando1 / operando2
+   
+def exponente(operando1, operando2):
+    return operando1 ** operando2
+
+funciones = {
+    "suma": suma,
+    "resta": resta,
+    "multiplicacion": multiplicacion,
+    "division": division,
+    "elevacion": exponente
+}
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        sys.exit("usage: [operador][operando1][operando2]")
+    _, funcion, op1, op2 = sys.argv
+
+
+    try:
+        operando1 = float(op1)
+        operando2 = float(op2)
+        solucion = funciones[operacion](operando1, operando2)
+        print(str(solucion))
+    except ValueError:
+        sys.exit("No estan permitidos operandos que no sean de tipo numerico")
